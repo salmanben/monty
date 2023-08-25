2f6b859c359929c5d5d896ce0fc2e97e3967242a
salmanben <salmanbenomar250@gmail.com>
2023-08-24 18:27:27 -0700

    Update pop_op.c file

--- pop_op.c
+++ pop_op.c
@@ -14,7 +14,7 @@ void pop_op(stack_t **stack, unsigned int line_number)
 
     if (*stack == NULL)
     {
-	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number)
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
         exit(EXIT_FAILURE);
     }
     else
